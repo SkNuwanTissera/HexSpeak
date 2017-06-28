5477db9f80ecc84d0657b0616b7a702d68961600
NuwanTissera <t-nuwant@brandix.com>
2017-06-28 10:58:16 +0530

    Sk - Main Routes

--- route.config.js
+++ route.config.js
@@ -1,22 +1,18 @@
 /**
  * Created by Sk on 6/27/2017.
  */
-var app = angular.module("PharmacyApp");
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/login", {
-            templateUrl : "public/login.ejs",
-            controller : "UserController"
-        }).when("/test", {
-        templateUrl : "test.html",
-        controller : "testController"
+var mainApp = angular.module("PharmacyApp", ['ngRoute']);
+
+mainApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+
+    when('/app/prescriptions', {
+        templateUrl: 'public/prescription.html', controller: 'PrescriptionController'
+    }).
+
+
+    otherwise({
+        redirectTo: '/login'
     });
-});
-app.controller("testController", function ($scope) {
-    $scope.msg = "ng-view is working fine";
-});
-/*
-* <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>
- <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
-*
-* */
+
+}]);
