c1283e9d373d4627c6280d4a727bc898e4c564fc
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2017-06-29 05:59:54 +0530

    sK pACKAGE cHANGE dONE NO ROUTING IS USED ;-( fRONTEND IS IN PUBLIC FOLDER

---
+++ public/app.module.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Sk on 6/29/2017.
+ */
+angular.module('PharmacyApp', []);
---
+++ public/route.config.js
@@ -0,0 +1,21 @@
+//
+// var app = angular.module('PharmacyApp', ['ngRoute']);
+//
+// app.config(function ($routeProvider, $locationProvider, $httpProvider) {
+//     $routeProvider
+//         .when('/prescription', {
+//             templateUrl: 'prescription.html',
+//             controller: 'PrescriptionController'
+//         })
+//         .when('/user', {
+//             templateUrl: 'views/home.html',
+//         })
+//         .otherwise({
+//             templateUrl: 'test.html',
+//             controller: 'testController'
+//         });
+//     $locationProvider.html5Mode(true);
+// });
+// app.controller("testController", function ($scope) {
+//     $scope.msg = "ng-view is working fine";
+// });
